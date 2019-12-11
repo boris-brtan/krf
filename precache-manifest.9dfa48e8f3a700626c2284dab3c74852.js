baa84b644ee4a857b91638228c6798f6a1bda587
Boris Brtáň <39240194+boris-brtan@users.noreply.github.com>
2019-12-11 21:07:27 +0100

    Updates

---
+++ precache-manifest.9dfa48e8f3a700626c2284dab3c74852.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acc5808e8c2fac6ce939f899ac0a8476",
+    "url": "/krf/index.html"
+  },
+  {
+    "revision": "ddf11e57d78b6880af4a",
+    "url": "/krf/static/css/main.b1707c8f.chunk.css"
+  },
+  {
+    "revision": "9d2d3fb6e94a0545b5fe",
+    "url": "/krf/static/js/2.f9e73fa1.chunk.js"
+  },
+  {
+    "revision": "ddf11e57d78b6880af4a",
+    "url": "/krf/static/js/main.83449e9d.chunk.js"
+  },
+  {
+    "revision": "69c344979c23a8783ef6",
+    "url": "/krf/static/js/runtime-main.7c35aa72.js"
+  }
+]);
